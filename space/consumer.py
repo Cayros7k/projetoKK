92f8d787e86b99c4ebb6df771056e3e7e12eddd3
Henrique Oliveira <contato.animous@gmail.com>
2023-12-05 03:01:39 -0300

    Modified

--- space/consumer.py
+++ space/consumer.py
@@ -1,36 +1,38 @@
-try:
-    from kafka import KafkaConsumer
-    import json
-    import requests
-    import os
-    import sys
-except Exception as e:
-    pass
+from kafka import KafkaConsumer
+import json
+import requests
+import os
 
-# ------------------------------
+# ---------------------------------------------------------
 os.environ['KAFKA_TOPIC'] = "FirstTopic"
 os.environ['SERVER_END_POINT'] = "http://localhost:9200"
-# -----------------------------
+# ---------------------------------------------------------
 
+# Classe para fazer upload de registros para o ElasticSearch a partir do Kafka.
 class ElasticSearchKafkaUploadRecord:
 
+    # Inicializa a classe com os dados necessários para fazer o upload.
     def __init__(self, json_data, hash_key, index_name):
-        self.hash_key = hash_key
-        self.json_data = json_data
-        self.index_name = index_name.lower()
+        self.hash_key = hash_key  # ---------------> Chave Hash para identificar o registro.
+        self.json_data = json_data  # -------------> Dados no formato JSON a serem enviados.
+        self.index_name = index_name.lower()  # ---> Nome do índice para o ElasticSearch (convertido para minúsculas).
 
+    # Faz o upload dos registros no cluster do ElasticSearch.
     def upload(self):
         """
         Uploads records on Elastic Search Cluster
         :return
         """
+        # Formata a URL para o endpoint do ElasticSearch com o nome do índice e a chave hash.
         URL = "{}/{}/_doc/{}".format(
             os.getenv("SERVER_END_POINT"), self.index_name, self.hash_key
-        )
+        ) 
         print(URL)
 
+        # Define os cabeçalhos para a requisição HTTP.
         headers = {"Content-Type": "application/json"}
 
+        # Faz uma requisição PUT para enviar os dados JSON para o ElasticSearch
         response = requests.request(
             "PUT", URL, headers=headers, data=json.dumps(self.json_data),
         )
@@ -38,14 +40,19 @@ class ElasticSearchKafkaUploadRecord:
 
         return {"status": 200, "data": {"message": "record uploaded to Elastic Search"}}
 
-
+# Função principal para consumir mensagens do Kafka e fazer upload para o ElasticSearch.
 def main():
 
+    # Cria um consumidor Kafka para o tópico especificado.
     consumer = KafkaConsumer(os.getenv("KAFKA_TOPIC"))
 
+    # Itera sobre as mensagens recebidas do tópico.
     for msg in consumer:
 
+        # Carrega os dados JSON recebidos na mensagem.
         payload = json.loads(msg.value)
+
+        # Adiciona metadados à carga útil, como informações do tópico, partição, offset, timestamp, etc.
         payload["meta_data"]={
             "topic":msg.topic,
             "partition":msg.partition,
@@ -55,10 +62,13 @@ def main():
             "key":msg.key,
         }
 
+        # Cria um objeto da classe ElasticSearchKafkaUploadRecord para enviar os dados para o ElasticSearch.
         helper = ElasticSearchKafkaUploadRecord(json_data=payload,
                                                 index_name=payload.get("meta_data").get("topic"),
                                                 hash_key=payload.get("meta_data").get("offset"),
                                                 )
+        
+        # Chama o método de upload para enviar os dados para o ElasticSearch.
         response = helper.upload()
 
 main()
--- space/producer.py
+++ space/producer.py
@@ -1,16 +1,17 @@
-try:
-    from kafka import KafkaProducer
-    from faker import Faker
-    import json
-    from time import sleep
-except Exception as e:
-    pass
+from kafka import KafkaProducer
+from faker import Faker
+import json
+from time import sleep
 
+# Cria uma instância de KafkaProducer que se conecta ao servidor Kafka na porta 9092.
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
+# Cria uma instância do Faker para gerar dados aleatórios.
 faker = Faker()
 
+# Loop que itera 50 vezes
 for _ in range(50):
+    # Gera dados aleatórios para serem enviados para o Kafka.
     _data = {
         "first_name": faker.first_name(),
         "city": faker.city(),
@@ -18,9 +19,15 @@ for _ in range(50):
         "state": faker.state(),
         "id":str(_)
     }
+
+    # Converte os dados gerados para um formato JSON codificado em UTF-8.
     _payload = json.dumps(_data).encode("utf-8")
+
+    # Envia os dados para o tópico 'FirstTopic' no servidor Kafka e guarda a resposta.
     response = producer.send('FirstTopic', _payload)
+
     print(response)
 
+    # Pausa o loop por 2 segundos antes de enviar a próxima mensagem.
     sleep(2)
 
